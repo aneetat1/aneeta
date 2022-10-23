4eeaf896ae31b1f4c25389447d640b60feaf7e49
Aneeta Thokkadam <40705473+aneetat1@users.noreply.github.com>
2022-10-22 23:04:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+
+
+img1.addEventListener("mouseover", img1func);
+img2.addEventListener("mouseover", img2func);
+img3.addEventListener("mouseover", img3func);
+
+function img1func() {
+    document.getElementById("fast-fact").innerHTML = "Generating electricity and heat by burning fossil fuels such as coal, oil, and natural gas causes a large chunk of global emissions. Most electricity is still produced from fossil fuels; only about a quarter comes from wind, solar, and other renewable sources.";
+}
+
+function img2func() {
+    document.getElementById("fast-fact").innerHTML = "Manufacturing and industry produce emissions, mostly from burning fossil fuels to produce energy for making things like cement, iron, steel, electronics, plastics, clothes, and other goods. Mining and other industrial processes also release gases.";
+}
+
+function img3func() {
+    document.getElementById("fast-fact").innerHTML = "Cutting down forests to create farms or pastures, or for other reasons, causes emissions, since trees, when they are cut, release the carbon they have been storing. Since forests absorb carbon dioxide, destroying them also limits nature’s ability to keep emissions out of the atmosphere.";
+}
